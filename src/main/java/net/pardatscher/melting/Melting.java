bae2bb153f2f28b65f71e3b01ef25ceff0421712
Michael Pardatscher <michael@pardatscher.net>
2020-02-18 16:18:14 +0100

    Initial project setup commit

---
+++ src/main/java/net/pardatscher/melting/Melting.java
@@ -0,0 +1,17 @@
+package net.pardatscher.melting;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Melting extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
